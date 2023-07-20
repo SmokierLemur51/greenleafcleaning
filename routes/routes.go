cdeb5740e5d0c7e7ad62b9dcfd82363fd971aeae
SmokierLemur51 <ldl6147@gmail.com>
2023-07-20 19:37:53 -0400

    stupid

--- routes/routes.go
+++ routes/routes.go
@@ -6,6 +6,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func LoginHandler(c *gin.Context){
+	c.HTML(http.StatusOK, "login.html", gin.H{
+		"title": "Greenleaf Cleaning",
+	})
+}
+
+
+
+
 func IndexHandler(c *gin.Context){
 	c.HTML(http.StatusOK, "index.html", gin.H{
 		"title": "Greenleaf Cleaning",
