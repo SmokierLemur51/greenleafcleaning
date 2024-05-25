f9d0940f71a115bc5d683571cf310e2c2052b04b
SmokierLemur51 <ldl6147@gmail.com>
2024-05-25 15:41:56 -0400

    love this

--- server/blueprints/public/views.py
+++ server/blueprints/public/views.py
@@ -4,6 +4,16 @@ from ...models.models import db, Supplier
 
 public = Blueprint('public', __name__, template_folder="templates/public", url_prefix="/")
 
+# Testing route
+@public.route("/test", methods=["GET","POST"])
+def test():
+    with current_app.app_context():
+        from ...models.populate import populate_suppliers
+        populate_suppliers(db)
+    return jsonify("pong")
+
+
+
 # 404 handler
 @public.errorhandler(404)
 def not_found_error(error):
@@ -20,16 +30,57 @@ def internal_error(error):
 @public.route("/")
 def index():
     elements = {
-        "title": "Greenleaf Cleaning",
+        "title": "Go-Go Gutters",
     }
     return render_template("index.html", elements=elements)
 
 
-# 
-@public.route("/test", methods=["GET","POST"])
-def test():
-    with current_app.app_context():
-        from ...models.populate import populate_suppliers
-        populate_suppliers(db)
-    return jsonify("pong")
+@public.route("/contact-us")
+def contact():
+    elements = {
+        "title": "Go-Go Gutters",
+    }
+    return render_template("contact.html", elements=elements)
 
+
+@public.route("/cleaning")
+def cleaning():
+    elements = {
+        "title": "Go-Go Gutters",
+    }
+    return render_template("cleaning.html", elements=elements)
+
+
+@public.route("/repairs")
+def repairs():
+    elements = {
+        "title": "Go-Go Gutters",
+    }
+    return render_template("repairs.html", elements=elements)
+
+
+@public.route("/replacements")
+def replacements():
+    elements = {
+        "title": "Go-Go Gutters",
+    }
+    return render_template("replacements.html", elements=elements)
+
+
+
+# thinking about splitting this into its own blueprint
+@public.route("/gutter-delivery")
+def gutter_delivery():
+    elements = {
+        "title": "Go-Go Gutters",
+    }
+    return render_template("gutter_delivery.html", elements=elements)
+
+
+
+@public.route("/gutter-guards")
+def gutter_guards():
+    elements = {
+        "title": "Go-Go Gutters",
+    }
+    return render_template("gutter_guards.html", elements=elements)
