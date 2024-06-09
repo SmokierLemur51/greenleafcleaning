fd8d7bbd0cf62f83e9ccccf816deafc8948f7fc2
SmokierLemur51 <ldl6147@gmail.com>
2024-06-09 18:29:46 -0400

    setting up a quart starting point

---
+++ backend/routes.py
@@ -0,0 +1,21 @@
+from quart import Quart, render_template
+
+
+async def configure_core_routes(app: Quart) -> None:
+    @app.route("/")
+    async def index():
+        elements = {'title': 'Success'}
+        return await render_template("index.html", elements=elements)
+
+    @app.route("/services")
+    async def services():
+        elements = {}
+        return await render_template("services.html", elements=elements)
+
+
+async def configure_test_routes(app: Quart):
+
+    @app.route("/schema/create")
+    async def create_tables():
+        pass
+
